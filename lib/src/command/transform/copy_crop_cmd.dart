f76de9e6090c2a5269c93e8d032ab6cabf79bea9
Dayona <dayonaart@gmail.com>
2023-03-18 10:49:39 +0700

    update from dayona

---
+++ lib/src/command/transform/copy_crop_cmd.dart
@@ -0,0 +1,29 @@
+import '../../transform/copy_crop.dart';
+import '../command.dart';
+
+class CopyCropCmd extends Command {
+  int x;
+  int y;
+  int width;
+  int height;
+  num radius;
+  bool antialias;
+
+  CopyCropCmd(Command? input,
+      {required this.x,
+      required this.y,
+      required this.width,
+      required this.height,
+      this.radius = 0,
+      this.antialias = false})
+      : super(input);
+
+  @override
+  Future<void> executeCommand() async {
+    await input?.execute();
+    final img = input?.outputImage;
+    outputImage = img != null
+        ? copyCrop(img, x, y, width, height, radius, antialias)
+        : null;
+  }
+}
